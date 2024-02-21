99914e6f4ac7545b0083c298f4a2f0b15f16f2a0
anastasiakezia <anastasiakezia08@gmail.com>
2024-02-21 08:06:57 +0700

    add menu for letter origin type and fix error

--- app/Http/Controllers/Admin/KaryawanController.php
+++ app/Http/Controllers/Admin/KaryawanController.php
@@ -3,24 +3,53 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Models\Karyawan;
+use App\Models\Department;
+use Yajra\DataTables\Facades\DataTables;
 
 class KaryawanController extends Controller
 {
     public function showUnit()
     {
     }
+    public function get()
+    {
+        if (request()->ajax()) {
+            $query = Department::with('departments')->get();
+
+            return Datatables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <a class="btn btn-primary btn-xs" data-bs-toggle="modal" data-bs-target="#updateModal' . $item->id . '">
+                            <i class="fas fa-edit"></i> &nbsp; Ubah
+                        </a>
+                        <form action="' . route('department.destroy', $item->id) . '" method="POST" onsubmit="return confirm(' . "'Anda akan menghapus item ini dari situs anda?'" . ')">
+                            ' . method_field('delete') . csrf_field() . '
+                            <button class="btn btn-danger btn-xs">
+                                <i class="far fa-trash-alt"></i> &nbsp; Hapus
+                            </button>
+                        </form>
+                    ';
+                })
+                ->addIndexColumn()
+                ->removeColumn('id')
+                ->rawColumns(['action'])
+                ->make();
+        }
+        $employees = Karyawan::with('departments')->get();
+        // $departments = Karyawan::with('departments')->get();
+        return response()->json(['employees' => $employees]);
+
+        // return view('pages.admin.employee.index', [
+        //     'employees' => $employees,
+        //     // 'departments' => $departments
+        // ]);
+    }
     public function dropdown_karyawan(Request $request)
     {
-        // $karyawan = Karyawan::where("departments_id", $request->id)->pluck('nama', 'id', 'position');
         $karyawan = Karyawan::where("departments_id", $request->id)->get();
-
-        // return response()->json(['res' => $karyawan]);
         return response()->json($karyawan);
-        // $karyawan = Karyawan::findOrFail($request->id);
-        // $department = $karyawan->departments()->pluck('id', 'nama', 'position');
-        // return response()->json(['data' => $department]);
-        // return response()->json($department);
     }
 }
